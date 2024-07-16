30443eb7ca8bf49a56447de65ff965f2ca8fcb1f
nayan <www.xil187@gmail.com>
2024-07-16 10:32:28 +0530

    Constructors implimented

---
+++ SecondaryConstructors/src/secondaryConstructor.kt
@@ -0,0 +1,42 @@
+package secondaryConstructor
+
+class GardenItem(val name: String)//primary constructor
+{
+    var material = name
+    var prize = 0
+    var category = "empty"
+
+    constructor(
+        name: String,
+        prize : Int
+    ) : this(name)//secondary / overloaded constructor
+    {
+        //here this(name) is a call to primary constructor which is required
+        material = name
+        this.prize = prize
+    }
+
+    constructor(
+        name : String,
+        prize : Int,
+        category : String
+    ) : this(name, prize)//secondary / overloaded constructor
+    {
+        material = name
+        this.prize = prize
+        this.category = category
+    }
+    fun content() : String
+    {
+        return "Object of $material is created with prize $prize and category $category ."
+    }
+}
+fun main()
+{
+    val item1 = GardenItem("Rose")//primary constructor invoked
+    val item2 = GardenItem("Blossom",2)//secondary / overloaded constructor invoked
+    val item3 = GardenItem("Marry",1,"Flower")//secondary / overloaded constructor invoked
+    println(item1.content())
+    println(item2.content())
+    println(item3.content())
+}
